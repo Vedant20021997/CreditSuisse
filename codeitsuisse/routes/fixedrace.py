146c81a3d75719f19d44c00c550488c95c55ec7f
Vedant20021997 <vedantm.2018@smu.edu.sg>
2021-09-25 21:19:18 +0800

    try

--- codeitsuisse/routes/fixedrace.py
+++ codeitsuisse/routes/fixedrace.py
@@ -14,6 +14,7 @@ def evaluate():
     list_data = list(data.split(','))
     string = ','
     formation = random.shuffle(list_data)
+    print(formation)
     return string.join(formation)
     
     # logging.info("My result :{}".format(result))
