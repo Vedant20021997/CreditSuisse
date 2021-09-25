4c68b59a777b43b2fcb32f5d538023db17e7435c
Vedant20021997 <vedantm.2018@smu.edu.sg>
2021-09-26 02:01:21 +0800

    d

--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -19,7 +19,7 @@ def decode():
     #possible_values = ["e", "d", "f"]
     choice = random.choices(possible_values, k=num_slots)
     print(choice)
-    return jsonify({"answer": ["u", "v", "u", "f", "v"]})
+    return jsonify({"answer": choice})
     
     # logging.info("My result :{}".format(result))
     # return json.dumps(result)
