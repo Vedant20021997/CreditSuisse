6e336ddedf16ff08bb43d3293ad1b4f36f4010f4
Vedant20021997 <vedantm.2018@smu.edu.sg>
2021-09-25 20:50:18 +0800

    try

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
-import codeitsuisse.routes.square
+# import codeitsuisse.routes.square
+# import codeitsuisse.routes.asteroid
+# import codeitsuisse.routes.tictactoe
+# import codeitsuisse.routes.stocks
+import codeitsuisse.routes.fixedrace
 
 
 
---
+++ codeitsuisse/routes/fixedrace.py
@@ -0,0 +1,21 @@
+import logging
+import json
+import random
+
+from flask import request, jsonify
+
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+@app.route('/fixedrace', methods=['POST'])
+def evaluate():
+    data = request.get_data(as_text=True)
+    list_data = data.split(',')
+    string = ''
+    formation = random.shuffle(list_data)
+    return string.join(formation)
+    
+    # logging.info("My result :{}".format(result))
+    # return json.dumps(result)
+
