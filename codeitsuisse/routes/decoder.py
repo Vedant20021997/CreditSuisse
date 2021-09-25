8cf13880e04fe63097e341c471807539d4676976
Vedant20021997 <vedantm.2018@smu.edu.sg>
2021-09-25 22:01:17 +0800

    okay

--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -10,11 +10,10 @@ logger = logging.getLogger(__name__)
 
 @app.route('/fixedrace', methods=['POST'])
 def evaluate():
-    data = request.get_data(as_text=True)
-    list_data = list(data.split(','))
-    random.shuffle(list_data)
-    return ','.join(list_data)
+    data = request.get_json()
+    possible_values = data["possible_values"]
+    num_slots = data["num_slots"]
+    return {"answer": random.choices(possible_values, k=num_slots)}
     
     # logging.info("My result :{}".format(result))
     # return json.dumps(result)
-
